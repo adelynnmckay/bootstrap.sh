e1588364195e31c8c00ce803f3d02af553fc2963
adelynnmckay <192640919+adelynnmckay@users.noreply.github.com>
2025-05-22 18:37:35 -0700

    reset versioning in formula

--- Formula/bootstrap.sh.rb
+++ Formula/bootstrap.sh.rb
@@ -2,9 +2,9 @@ class BootstrapSh < Formula
   desc "ade's bootstrap script. aka the bootrap.sh build system. have fun."
   homepage "https://github.com/adelynnmckay/bootstrap.sh"
   license "MIT"
-  sha256 "4025c251fcce9d685163ed21db28d6c6ba64267ff7ce40716cb38ad16bdd4c1b"
-  url "https://github.com/adelynnmckay/bootstrap.sh/archive/refs/tags/v0.0.4.tar.gz"
-  version "0.0.4"
+  sha256 ""
+  url ""
+  version ""
 
   def install
     bin.install "bootstrap.sh" => "bootstrap.sh"
